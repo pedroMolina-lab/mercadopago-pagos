18fb5eceb87cd1a8376f60c7e5d9fbb66d3c0e1a
piter <pedri_molina@hotmail.com>
2024-06-27 01:28:42 -0300

    funcionandoResend

--- lib/resend.ts
+++ lib/resend.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.KEY_MAIL);
 
 export async function sendEmail(email, code) {
   const envioEmail = await resend.emails.send({
-    from: 'pedromolina.online',
+    from: 'onboarding@pedromolina.online',
     to: email,
     subject: "codigo de acceso",
     html: code
